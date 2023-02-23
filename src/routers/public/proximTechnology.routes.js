6e0cf7aca51201650808b9ba8f967716f90fc8f3
vdevalero <vdevalero44@gmail.com>
2023-02-23 09:29:31 +0100

    correction url

--- src/routers/public/proximTechnology.routes.js
+++ src/routers/public/proximTechnology.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { pool } from "../../db.js";
 import { getOneProximTechnologie } from "../../controllers/public/users.controller.js";
 const router = Router();
-router.get(":id", getOneProximTechnologie);
+router.get("/:id", getOneProximTechnologie);
 router.get("/", async (req, res) => {
   const [rows] = await pool.query("SELECT * FROM proxim_technology");
   res.json(rows);
